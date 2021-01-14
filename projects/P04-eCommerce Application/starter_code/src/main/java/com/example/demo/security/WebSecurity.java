8227d358517e4c4ac80172fb64a13fd99ce728ba
ginowine <ginowine1@gmail.com>
2021-01-14 17:13:02 +0100

    create user and login works

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/security/WebSecurity.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/security/WebSecurity.java
@@ -48,10 +48,4 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         //auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-//        return source;
-//    }
 }
