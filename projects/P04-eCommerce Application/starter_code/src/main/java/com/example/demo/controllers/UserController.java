df77f1470c5e23d63070e0a6c3c2d55494ed6f59
ginowine <ginowine1@gmail.com>
2021-01-17 10:53:16 +0100

    logger implemented

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/user")
 public class UserController {
 
-	//static final Logger logger = Logger.getLogger(UserController.class.getName());
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -51,8 +53,8 @@ public class UserController {
 		//logger.info("Creating user {}", createUserRequest.getUsername());
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
+		logger.info("User name set with", createUserRequest.getUsername());
 		Cart cart = new Cart();
-
 		if (createUserRequest.getPassword().length() < 7 ||
 				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
 			//logger.error("error with user password. cannot create user{}", createUserRequest.getUsername());
